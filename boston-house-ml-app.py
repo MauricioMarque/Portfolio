3a0c023215875d8ca2f444507bcf1c0cdec5bc97
Maurício de Araújo Marques <123465270+MauricioMarque@users.noreply.github.com>
2024-04-21 19:30:46 -0300

    Add files via upload

--- boston-house-ml-app.py
+++ boston-house-ml-app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import shap
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
@@ -69,19 +68,4 @@ prediction = model.predict(df)
 st.header('Prediction of MEDV')
 st.write(prediction)
 st.write('---')
-
-# Explaining the model's predictions using SHAP values
-# https://github.com/slundberg/shap
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
-st.write('---')
-
-plt.title('Feature importance based on SHAP values (Bar)')
-shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight')
 
